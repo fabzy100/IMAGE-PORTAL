10c7ef277f20dd26c3fa109e612117e85e870210
Adesanya Emmanuel <166428794+Imma-Nuel-1@users.noreply.github.com>
2024-10-21 16:40:25 +0100

    Delete image-portal-backend directory

--- image-portal-backend/db.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require("dotenv");
-const mongoose = require("mongoose");
-dotenv.config();
-
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
-const db = mongoose.connection;
-
-module.exports = db;
